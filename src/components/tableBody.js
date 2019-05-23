b42d787ae418c2ba487ceac70c558c1d0990016d
Javier Cardenas <neo1807@gmail.com>
2019-05-23 18:38:06 -0500

    chore: create row component

--- src/components/tableBody.js
+++ src/components/tableBody.js
@@ -1,38 +1,12 @@
 import React from "react";
-import { FiMoreHorizontal } from "react-icons/fi";
+import TableRow from "./tableRow";
 export default class TableBody extends React.Component {
   render() {
     return (
       <tbody>
-        <tr>
-          <td>
-            <div className="lead-avatar">AJ</div>
-            <div className="lead">
-              Ángela Jimínez
-              <br />
-              <small>Case: Evasion</small>
-            </div>
-          </td>
-          <td className="email">MarcoAntonio_Montaez21@gmail.com</td>
-          <td>951.702.467</td>
-          <td>
-            <select>
-              <option value="1">Internet search</option>
-              <option value="2">Facebook</option>
-              <option value="3">Referral</option>
-            </select>
-          </td>
-          <td>
-            <select>
-              <option value="1">Lead</option>
-              <option value="2">Client</option>
-              <option value="3">Prospect</option>
-            </select>
-          </td>
-          <td>
-            <FiMoreHorizontal />
-          </td>
-        </tr>
+        <TableRow />
+        <TableRow />
+        <TableRow />
       </tbody>
     );
   }
---
+++ src/components/tableRow.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { FiMoreHorizontal } from "react-icons/fi";
+export default class TableRow extends React.Component {
+  render() {
+    return (
+      <tr>
+        <td>
+          <div className="lead-avatar">AJ</div>
+          <div className="lead">
+            Ángela Jimínez
+            <br />
+            <small>Case: Evasion</small>
+          </div>
+        </td>
+        <td className="email">MarcoAntonio_Montaez21@gmail.com</td>
+        <td>951.702.467</td>
+        <td>
+          <select>
+            <option value="1">Internet search</option>
+            <option value="2">Facebook</option>
+            <option value="3">Referral</option>
+          </select>
+        </td>
+        <td>
+          <select>
+            <option value="1">Lead</option>
+            <option value="2">Client</option>
+            <option value="3">Prospect</option>
+          </select>
+        </td>
+        <td>
+          <FiMoreHorizontal />
+        </td>
+      </tr>
+    );
+  }
+}
