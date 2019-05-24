72869d5d732cd5dc6455fee660805a3a6f1527d3
Javier Cardenas <neo1807@gmail.com>
2019-05-23 19:12:42 -0500

    feat: view header styles

--- src/components/controls.js
+++ src/components/controls.js
@@ -1,6 +1,27 @@
 import React from "react";
+import { FiSearch, FiChevronDown, FiPlus } from "react-icons/fi";
 export default class Controls extends React.Component {
   render() {
-    return <div>controles</div>;
+    return (
+      <div className="view-header">
+        <div className="view-title">
+          <h1>Leads</h1>Showing <span>20</span> out of 521 contacts
+        </div>
+        <div className="view-controls">
+          <FiSearch />
+          <div className="view-actions">
+            <div>
+              Type <FiChevronDown />
+            </div>
+            <div>
+              Source <FiChevronDown />
+            </div>
+            <button>
+              <FiPlus /> add contact
+            </button>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
